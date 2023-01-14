4d579c2f52aa9462299d5f6daab71287c5fafc8b
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-14 13:54:07 +0300

    56

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿/* Задача 54: Задайте двумерный массив. Напишите программу, 
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу,  
 которая упорядочит по убыванию элементы каждой строки двумерного массива.
 Например, задан массив:
 1 4 7 2
@@ -7,7 +7,7 @@
 В итоге получается вот такой массив:
 7 4 2 1
 9 5 3 2
-8 4 4 2 */
+8 4 4 2 
 
 int[,] array = new int[4, 4];
 for (int i = 0; i < array.GetLength(0); i++) //1
@@ -43,3 +43,48 @@ for (int i = 0; i < array.GetLength(0); i++) //3
     }
     Console.WriteLine();
 }
+*/
+
+/* Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+int[,] array = new int[4, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int summ = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        summ += array[i, j];
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine($"сумма = {summ}");
+}
+
+int minstring = 1;
+int minimum = 0;
+
+for (int i = 0; i < array.GetLength(1); i++)
+    minimum += array[0, i];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+        sum += array[i, j];
+    if (sum < minimum)
+    {
+        minimum = sum;
+        minstring = i + 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"строка № {minstring}");
+
+
